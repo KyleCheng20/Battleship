3c9e760dac7304bbb5e79d9e9c105c8c0f40a8d2
Kyle Cheng <kcheng120@gmail.com>
2026-03-09 18:48:25 -0400

    Show invalid preview placement if placing ships out of bounds or overlapping another placed ship

--- src/modules/dom/shipHoverPreview.js
+++ src/modules/dom/shipHoverPreview.js
@@ -1,16 +1,34 @@
 export function clearPreview(board){
-    const previewCells = board.querySelectorAll(".preview");
-    previewCells.forEach(cell => cell.classList.remove("preview"));
+    const previewCells = board.querySelectorAll(".preview, .preview-invalid");
+    previewCells.forEach(cell => {
+        cell.classList.remove("preview");
+        cell.classList.remove("preview-invalid");
+    });
+   
 }
 
 export function showPreview(board, x, y, length){
     clearPreview(board);
 
+    let isValid = true;
+    const cells = [];
+
     for(let i = 0; i < length; i++){
-        const previewCell = board.querySelector(`[data-x="${x}"][data-y="${y + i}"]`);
+        const cell = board.querySelector(`[data-x="${x}"][data-y="${y + i}"]`);
 
-        if(!previewCell) break;
+        if(!cell){
+            isValid = false;
+            continue;
+        }
 
-        previewCell.classList.add("preview");
+        if(cell.classList.contains("ship")) isValid = false;
+
+        cells.push(cell);
     }
+
+    cells.forEach(cell => {
+        if(!cell) return;
+
+        cell.classList.add(isValid ? "preview" : "preview-invalid");
+    });
 }
